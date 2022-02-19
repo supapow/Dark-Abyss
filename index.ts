2ba689485b58fc65341f15904be389da390781f5
supapow <ola.holmgren@gmail.com>
2022-02-19 12:08:42 +0100

    Initial commit

--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
 import * as path from 'path';
 import { generateTheme, IColorSet } from 'vscode-theme-generator';
 
-const themeName = 'Generated';
-const colors = {
+const themeName = 'Dark Abyss';
+const colors = { 
+  abyss: '#0d0e14',
   black: '#111111',
-  white: '#FFFFFF',
-  blue: '#5555FF',
-  red: '#FF5555',
-  green: '#55FF55',
-  yellow: '#FFFF33'
+  white: '#96d4ff',
+  blue: '#2faad4',
+  red: '#30b6ff',
+  green: '#e8c77b',
+  yellow: '#dd398d'
 }
 
 const colorSet: IColorSet = {
   base: {
-    background: colors.black,
+    background: colors.abyss,
     foreground: colors.white,
     color1: colors.blue,
     color2: colors.red,
